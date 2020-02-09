2737d2e00865f3fdf5a3dbeaae99e1a9668a6e58
fchabin <faycal.chabini@gmail.com>
2020-02-09 21:04:44 +0100

    Error fix interfacalias

--- AD-DCinstall.ps1
+++ AD-DCinstall.ps1
@@ -3,101 +3,114 @@
 {
 
 
-    param(
+param(
 
     
 
-        [parameter(Mandatory = $true)]
-        [pscredential]$domainCred,
+    [parameter(Mandatory=$true)]
+    [pscredential]$domainCred,
 
-        [parameter(Mandatory = $true)]
-        [pscredential]$safemodeAdministratorCred
+    [parameter(Mandatory=$true)]
+    [pscredential]$safemodeAdministratorCred
 
-    )
+)
 
-    Import-Module PSDesiredStateConfiguration
-    Import-DscResource -ModuleName xActiveDirectory
-    Import-DscResource –ModuleName PSDesiredStateConfiguration
-    Import-DscResource -ModuleName xNetworking
-    Import-DscResource -ModuleName xdhcpserver
+Import-Module PSDesiredStateConfiguration
+Import-DscResource -ModuleName xActiveDirectory
+Import-DscResource –ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xNetworking
+Import-DscResource -Module xComputerManagement
+Import-DscResource -ModuleName xdhcpserver
 
 
-    Node $AllNodes.Where{ $_.Role -eq "Primary DC" }.NodeName
+Node $AllNodes.Where{$_.Role -eq "Primary DC"}.NodeName
     {
-        LocalConfigurationManager {
-            ActionAfterReboot  = 'ContinueConfiguration'
-            ConfigurationMode  = 'ApplyOnly'
+      LocalConfigurationManager
+        {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
             RebootNodeIfNeeded = $true
         }
 
-        xIPAddress NewIPAddress
+     xIPAddress NewIPAddress
         {
             IPAddress      = "192.168.0.220/24"
-            InterfaceAlias = "Ethernet"
+            InterfaceAlias = 'Ethernet'
             AddressFamily  = "IPV4"
         }
 
-        xDefaultGatewayAddress DefaultGateway {
-            Address        = $Node.DefaultGateway
-            InterfaceAlias = $Node.InterfaceAlias
-            AddressFamily  = "IPV4"
-        }
-        WindowsFeature DHCP {
-            DependsOn            = '[xIPAddress]NewIpAddress'
-            Name                 = 'DHCP'
-            Ensure               = 'PRESENT'
+     xDefaultGatewayAddress DefaultGateway
+        {
+            Address = $Node.DefaultGateway
+            InterfaceAlias = 'Ethernet'
+            AddressFamily = "IPV4"
+         }
+
+     
+      WindowsFeature DHCP 
+        {
+            DependsOn = '[xIPAddress]NewIpAddress'
+            Name = 'DHCP'
+            Ensure = 'PRESENT'
             IncludeAllSubFeature = $true                                                                                                                              
  
         }  
-        xDhcpServerScope Scope {
-            IPStartRange  = "192.168.0.223"
-            IPEndRange    = "192.168.0.227"
-            Name          = "TestScope1"
-            SubnetMask    = "255.255.255.240"
-            State         = "Active"            
-            Ensure        = "Present"
+
+        xDhcpServerScope Scope
+        {
+            IPStartRange = "192.168.0.223"
+            IPEndRange = "192.168.0.227"
+            Name = "TestScope1"
+            SubnetMask = "255.255.255.240"
+            State = "Active"            
+            Ensure = "Present"
             LeaseDuration = "7:00:00"
-            DependsOn     = "[WindowsFeature]DHCP"
+            DependsOn = "[WindowsFeature]DHCP"
             
         }
+        
  
-        WindowsFeature DNS { 
+     WindowsFeature DNS 
+        { 
             Ensure = "Present" 
-            Name   = "DNS"
+            Name = "DNS"
         }
 
-        xDnsServerAddress DnsServerAddress { 
+        xDnsServerAddress DnsServerAddress 
+        { 
             Address        = '127.0.0.1' 
             InterfaceAlias = 'Ethernet'
             AddressFamily  = 'IPv4'
-            DependsOn      = "[WindowsFeature]DNS"
+            DependsOn = "[WindowsFeature]DNS"
         }
-
-        File ADFiles {
+      File ADFiles
+        {
             DestinationPath = 'C:\NTDS'
-            Type            = 'Directory'
-            Ensure          = 'Present'
+            Type = 'Directory'
+            Ensure = 'Present'
         }
      
-        WindowsFeature ADDSInstall {
-            Ensure = "Present"
-            Name   = "AD-Domain-Services"
-        }
-
-        WindowsFeature ADDSTools {
-            Ensure = 'Present'
-            Name   = 'RSAT-ADDS'
-        }
-
-        xADDomain FirstDC
+      WindowsFeature ADDSInstall
         {
-            DomainName                    = "bvbamoos.local"
-            DomainNETBIOSName             = "BVBAMOOS"
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+      WindowsFeature ADDSTools
+        {
+            Ensure='Present'
+            Name = 'RSAT-ADDS'
+        }
+
+      xADDomain FirstDC
+        {
+            DomainName = "bvbamoos.local"
+            DomainNETBIOSName = "BVBAMOOS"
             DomainAdministratorCredential = $domainCred
             SafemodeAdministratorPassword = $safemodeAdministratorCred
-            DatabasePath                  = 'C:\NTDS'         
-            LogPath                       = 'C:\NTDS' 
-            DependsOn                     = "[WindowsFeature]ADDSInstall"
+            DatabasePath = 'C:\NTDS'         
+            LogPath = 'C:\NTDS' 
+            DependsOn = "[WindowsFeature]ADDSInstall"
         }
 
     }#Node
@@ -109,14 +122,14 @@
 $ADConfig = @{
     AllNodes = @(
         @{
-            NodeName                    = "localhost"
-            Role                        = "Primary DC"
-            DomainName                  = "bvbamoos.local"
-            RetryCount                  = 20
-            RetryIntervalSec            = 30
+            NodeName = "localhost"
+            Role = "Primary DC"
+            DomainName = "bvbamoos.local"
+            RetryCount = 20
+            RetryIntervalSec = 30
             PsDscAllowPlainTextPassword = $true
-            PSDscAllowDomainUser        = $true
-        }
+            PSDscAllowDomainUser = $true
+         }
     )
 }
 
