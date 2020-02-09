5edeb413b61c6c9e322e7612c7edbd53cdf27057
Administrator@BVBAMOOS.LOCAL <Administrator@BVBAMOOS.LOCAL>
2020-02-09 05:17:41 -0800

    AD-DC added + promoted

---
+++ AD-DCinstall.ps1
@@ -0,0 +1,88 @@
+﻿Configuration NewDomain 
+
+{
+
+
+param(
+
+    
+
+    [parameter(Mandatory=$true)]
+    [pscredential]$domainCred,
+
+    [parameter(Mandatory=$true)]
+    [pscredential]$safemodeAdministratorCred
+
+)
+
+Import-Module PSDesiredStateConfiguration
+Import-DscResource -ModuleName xActiveDirectory
+Import-DscResource –ModuleName PSDesiredStateConfiguration
+
+
+Node $AllNodes.Where{$_.Role -eq "Primary DC"}.NodeName
+    {
+      LocalConfigurationManager
+        {
+            ActionAfterReboot = 'ContinueConfiguration'
+            ConfigurationMode = 'ApplyOnly'
+            RebootNodeIfNeeded = $true
+        }
+
+      File ADFiles
+        {
+            DestinationPath = 'C:\NTDS'
+            Type = 'Directory'
+            Ensure = 'Present'
+        }
+     
+      WindowsFeature ADDSInstall
+        {
+            Ensure = "Present"
+            Name = "AD-Domain-Services"
+        }
+
+      WindowsFeature ADDSTools
+        {
+            Ensure='Present'
+            Name = 'RSAT-ADDS'
+        }
+
+      xADDomain FirstDC
+        {
+            DomainName = "bvbamoos.local"
+            DomainNETBIOSName = "BVBAMOOS"
+            DomainAdministratorCredential = $domainCred
+            SafemodeAdministratorPassword = $safemodeAdministratorCred
+            DatabasePath = 'C:\NTDS'         
+            LogPath = 'C:\NTDS' 
+            DependsOn = "[WindowsFeature]ADDSInstall"
+        }
+
+    }#Node
+
+}#Config Closing
+
+#AD Config
+
+$ADConfig = @{
+    AllNodes = @(
+        @{
+            NodeName = "localhost"
+            Role = "Primary DC"
+            DomainName = "bvbamoos.local"
+            RetryCount = 20
+            RetryIntervalSec = 30
+            PsDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser = $true
+         }
+    )
+}
+
+NewDomain -ConfigurationData $ADConfig `
+    -safemodeAdministratorCred (Get-Credential -UserName '(Administrator)'  `
+        -Message "New Domain Safe Mode Administrator Password") `
+    -domainCred (Get-Credential -UserName bvbamoos.local\administrator `
+        -Message "New Domain Admin Credential") `
+
+Set-DscLocalConfigurationManager -Path .\NewDomain -Verbose -Force
