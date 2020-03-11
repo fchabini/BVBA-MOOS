9ed91559bc32cd72f7f45fdba1c77e9d7e53f2ba
fchabini <faycal.chabini@example.com>
2020-03-11 18:35:21 +0100

    hashf files

---
+++ LCMconfigration.ps1
@@ -0,0 +1,19 @@
+﻿[DSCLocalConfigurationManager()]
+configuration LCMBVBAMOOS
+{
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Push'
+            RebootNodeIfNeeded = $True
+RefreshFrequencyMins =             30
+ConfigurationMode = 'ApplyAndAutoCorrect'
+
+        }
+    }
+}
+LCMBVBAMOOS
+
+LCMBVBAMOOS -outputpath "C:\Users\Administrator\NewDomain\Localhostconfig"
+Set-DscLocalConfigurationManager -Path "C:\Users\Administrator\NewDomain\Localhostconfig"
